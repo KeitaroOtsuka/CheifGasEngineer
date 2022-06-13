6565cadfb147a855e72d7e3ecb78ca6987fd6fe9
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-06-13 17:13:07 +0900

    flash_message途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
+  add_flash_types :success, :info, :warning, :danger
 
   private
 
