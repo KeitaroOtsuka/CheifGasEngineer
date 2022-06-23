c3a161011a3068929af54b41e4619f83b32fba9c
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-06-23 13:21:50 +0900

    Question一覧と詳細ページをとりあえず作成

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :exam
-  has_many :choices
+  has_many :choices, dependent: :destroy
 
   has_many :result_questions, dependent: :destroy
   has_many :results, through: :group_users, dependent: :destroy
