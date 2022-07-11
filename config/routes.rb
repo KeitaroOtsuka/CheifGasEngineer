807610dac73c0f14e326774c83be766484647b6c
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-07-11 15:33:07 +0900

    新フォームのビューページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,6 @@ Rails.application.routes.draw do
   resources :results do
     resources :result_choices, only: %i[create], shallow: true
   end
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
