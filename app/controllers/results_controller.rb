f16937721662fb87e4fcd994757cf3c5b1a90201
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-07-16 00:27:19 +0900

    途中２

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,14 +1,13 @@
 class ResultsController < ApplicationController
   def create
     @result = current_user.results.build(question_id: params[:question_id])
-    if @result.save
-      if params[:choice_number].to_i == params[:question_id].answer
-        @judge = true
-      else
-        @judge = false
-      end
-      @result_choice = @result.result_choices.build(result_id: @result.id, choice_id: params[:choice_id])
-      @result_choice.save
+    @result.save
+    if params[:choice_number].to_i == Question.find(params[:question_id]).answer
+      @judge = true
+    else
+      @judge = false
     end
+    @result_choice = @result.result_choices.build(result_id: @result.id, choice_id: params[:choice_id], judge: @judge)
+    @result_choice.save
   end
 end
