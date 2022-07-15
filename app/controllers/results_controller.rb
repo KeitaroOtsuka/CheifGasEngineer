9ad87f94585bfcbb373f1f949e392edb83762d9f
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-07-16 01:22:44 +0900

    resultControllerテスト

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -9,5 +9,6 @@ class ResultsController < ApplicationController
     end
     @result_choice = @result.result_choices.build(result_id: @result.id, choice_id: params[:choice_id], judge: @judge)
     @result_choice.save
+    binding.pry
   end
 end
