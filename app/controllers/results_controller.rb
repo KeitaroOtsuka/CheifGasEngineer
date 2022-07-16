0310126f8a8c2c033e1796d66207d7dce8c8d942
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-07-16 19:54:36 +0900

    question_controllerをrestfulな使用にしないといけない

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -9,5 +9,7 @@ class ResultsController < ApplicationController
     end
     @result_choice = @result.result_choices.build(result_id: @result.id, choice_id: params[:choice_id], judge: @judge)
     @result_choice.save
+    redirect_to root_path
+    binding.pry
   end
 end
