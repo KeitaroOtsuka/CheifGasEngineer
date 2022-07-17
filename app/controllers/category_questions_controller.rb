ee7ef03adfe10fc661bdfbed655d8b92f27e12bf
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-07-17 19:50:07 +0900

    コントローラ分け終わった

--- app/controllers/category_questions_controller.rb
+++ app/controllers/category_questions_controller.rb
@@ -6,22 +6,17 @@ class CategoryQuestionsController < ApplicationController
   def show
     # binding.pry
     if params[:exam_id].to_i == 1
-      a = Question.where(exam_id: 1).order("RANDOM()").limit(16)
-      b = Question.where(exam_id: 2).order("RANDOM()").limit(10)
-      c = Question.where(exam_id: 3).order("RANDOM()").limit(20)
-    elsif params[:exam_id].to_i == 4
-      a = Question.where(exam_id: 4).order("RANDOM()").limit(16)
-      b = Question.where(exam_id: 5).order("RANDOM()").limit(10)
-      c = Question.where(exam_id: 6).order("RANDOM()").limit(20)
+      @questions = Question.where(exam_id: 1).or(Question.where(exam_id: 4)).order("RANDOM()").limit(16)
+    elsif params[:exam_id].to_i == 2
+      @questions = Question.where(exam_id: 2).or(Question.where(exam_id: 5)).order("RANDOM()").limit(10)
+    elsif params[:exam_id].to_i == 3
+      @questions = Question.where(exam_id: 3).or(Question.where(exam_id: 6)).order("RANDOM()").limit(20)
     elsif params[:exam_id].to_i == 7
-      a = Question.where(exam_id: 7).order("RANDOM()").limit(16)
-      b = Question.where(exam_id: 8).order("RANDOM()").limit(10)
-      c = Question.where(exam_id: 9).order("RANDOM()").limit(20)
-    elsif params[:exam_id].to_i == 10
-      a = Question.where(exam_id: 10).order("RANDOM()").limit(16)
-      b = Question.where(exam_id: 11).order("RANDOM()").limit(10)
-      c = Question.where(exam_id: 12).order("RANDOM()").limit(20)
+      @questions = Question.where(exam_id: 7).or(Question.where(exam_id: 10)).order("RANDOM()").limit(16)
+    elsif params[:exam_id].to_i == 8
+      @questions = Question.where(exam_id: 8).or(Question.where(exam_id: 11)).order("RANDOM()").limit(10)
+    elsif params[:exam_id].to_i == 9
+      @questions = Question.where(exam_id: 9).or(Question.where(exam_id: 12)).order("RANDOM()").limit(20)
     end
-    @questions = a + b + c
   end
 end
