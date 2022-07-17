a12fa99a46561bb332fab2746b962e5fcf797d60
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-07-17 16:10:33 +0900

    一旦コミット

--- app/controllers/year_questions_controller.rb
+++ app/controllers/year_questions_controller.rb
@@ -4,6 +4,25 @@ class YearQuestionsController < ApplicationController
   end
 
   def show
+    show_questions = Question.find(params[:exam_id]).all
+    if show_questions.exam_id == 1 || 2 || 3 ?
+      a = Question.where(exam_id: 1).order("RANDOM()").limit(16)
+      b = Question.where(exam_id: 2).order("RANDOM()").limit(10)
+      c = Question.where(exam_id: 3).order("RANDOM()").limit(20)
+    elsif show_questions.exam_id == 4 || 5 || 6 ?
+      a = Question.where(exam_id: 4).order("RANDOM()").limit(16)
+      b = Question.where(exam_id: 5).order("RANDOM()").limit(10)
+      c = Question.where(exam_id: 6).order("RANDOM()").limit(20)
+    elsif show_questions.exam_id == 7 || 8 || 9 ?
+      a = Question.where(exam_id: 7).order("RANDOM()").limit(16)
+      b = Question.where(exam_id: 8).order("RANDOM()").limit(10)
+      c = Question.where(exam_id: 9).order("RANDOM()").limit(20)
+    else
+      a = Question.where(exam_id: 10).order("RANDOM()").limit(16)
+      b = Question.where(exam_id: 11).order("RANDOM()").limit(10)
+      c = Question.where(exam_id: 12).order("RANDOM()").limit(20)
+    end
+    @questions = a + b + c
   end
   
   def show_reiwa2_otsu
