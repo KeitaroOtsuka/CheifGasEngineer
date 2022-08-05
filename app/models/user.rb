976996e11afcdceccd5c54cc3a0dd33880459049
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-08-05 23:16:50 +0900

    管理者ページ修正要

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
+  validates :comment, length: { maximum: 255 }
 
   validates :email, uniqueness: true
 
